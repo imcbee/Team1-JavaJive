2e129635ed161616c9a5ea15055b999f50812501
imcbee <imcbee@terpmail.umd.edu>
2023-02-16 00:27:28 +0000

    new folder

---
+++ MCCTeam1JavaJive/src/main/java/com/bah/mcc/day1/domain/Customer.java
@@ -0,0 +1,51 @@
+package com.bah.mcc.day1.domain;
+
+public class Customer {
+
+		private Long id;
+		private String name;
+		private String password;
+		private String email;
+		
+		public Customer(Long id, String name, String password, String email) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.password = password;
+			this.email = email;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+		
+}
---
+++ MCCTeam1JavaJive/src/test/java/com/bah/demo/MccTeam1JavaJiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccTeam1JavaJiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
