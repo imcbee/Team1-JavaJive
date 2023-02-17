0e1e0d5f31f98e4592f3081dec6863069406eed8
wasadmin <Tatwadhia_Ankush@bah.com>
2023-02-17 22:58:17 +0000

    Fixed event api get methods

--- MCCTeam1JavaJive/src/main/java/com/bah/mcc/day1/api/EventAPI.java
+++ MCCTeam1JavaJive/src/main/java/com/bah/mcc/day1/api/EventAPI.java
@@ -1,14 +1,16 @@
 package com.bah.mcc.day1.api;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,40 +23,19 @@ import com.bah.mcc.day1.repository.EventsRepository;
 @RestController
 @RequestMapping("/events")
 public class EventAPI {
-	ArrayList<Event> eventList = new ArrayList<Event>();
 	
 	@Autowired
 	EventsRepository repo;
-	
-
-//	public EventAPI() {
-//		Event e1 = new Event(1, "CNF001", "All-Java Conference", "Lectures and exhibits covering all Java topics");
-//		Event e2 = new Event(2, "WKS002", "Spring Boot Workshop", "Hands-on Spring Boot Workshop");
-//		Event e3 = new Event(3, "TRN001", "Angular Training Course", "Five day introductory training in Angular");
-//		Event e4 = new Event(4, "RNR004", "Rock n Roll Concert", "BAH Employees RocknRoll Concert");
-//		
-//		eventList.add(e1);
-//		eventList.add(e2);
-//		eventList.add(e3);
-//		eventList.add(e4);
-//		
-//	}
 
 	@GetMapping
-	public Collection<Event> getAll() {
-		return this.eventList;
+	public Iterable<Event> getAll() {
+		return repo.findAll();
 	}
 
 	@GetMapping("/{eventId}")
-	public Event getEventById(@PathVariable("eventId") long id) {
+	public Optional<Event> getEventById(@PathVariable("eventId") long id) {
 		
-		Event event = null;
-		for (int i = 0; i < eventList.size(); i++) {
-			if (eventList.get(i).getId() == id) {
-				event = eventList.get(i);
-			}
-		}
-		return event;
+		return repo.findById(id);
 	}
 	
 	@PostMapping
@@ -75,5 +56,27 @@ public class EventAPI {
 		return response;
 		
 	}
+	
+	@PutMapping("/{eventId}")
+	public ResponseEntity<?> putEvent(@RequestBody Event newEvent, @PathVariable("eventId") long eventId) 
+	{
+		if (newEvent.getId() != eventId || newEvent.getCode() == null || newEvent.getTitle() == null || newEvent.getDescription() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		newEvent = repo.save(newEvent);
+		ResponseEntity<?> response = ResponseEntity.ok().build();
+		
+		return response;
+	}	
+	
+	@DeleteMapping("/{eventId}")
+	public ResponseEntity<?> deleteEventById(@PathVariable("eventId") long id) {
+		repo.deleteById(id);
+		
+		ResponseEntity<?> response = ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		
+		return response;
+	}
+	
 	
 }
