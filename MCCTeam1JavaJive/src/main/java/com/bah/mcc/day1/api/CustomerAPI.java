8b304548ba90028caad4e0645c97cc6016fadd84
Austin <austinschreck@yahoo.com>
2023-02-17 21:51:17 +0000

    modified CustomerAPI and Customer.java

--- MCCTeam1JavaJive/src/main/java/com/bah/mcc/day1/api/CustomerAPI.java
+++ MCCTeam1JavaJive/src/main/java/com/bah/mcc/day1/api/CustomerAPI.java
@@ -1,54 +1,108 @@
 package com.bah.mcc.day1.api;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.bah.mcc.day1.domain.Customer;
+import com.bah.mcc.day1.repository.CustomersRepository;
+
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerAPI {
-	ArrayList<Customer> customerList = new ArrayList<Customer>();
-
-	public CustomerAPI() {
-		Customer c1 = new Customer(1, "Austin", "pass", "austin@bah.com");
-		Customer c2 = new Customer(2, "Anne", "pass", "anne@bah.com");
-		Customer c3 = new Customer(3, "Timothy", "pass", "timothy@bah.com");
-		Customer c4 = new Customer(4, "Emily", "pass", "emily@bah.com");
-		Customer c5 = new Customer(5, "John", "pass", "john@bah.com");
-		Customer c6 = new Customer(6, "Morgan", "pass", "morgan@bah.com");
-		Customer c7 = new Customer(7, "Yerarmy", "pass", "Yerarmy@bah.com");
-		Customer c8 = new Customer(8, "Brian", "pass", "brian@bah.com");
-
-		customerList.add(c1);
-		customerList.add(c2);
-		customerList.add(c3);
-		customerList.add(c4);
-		customerList.add(c5);
-		customerList.add(c6);
-		customerList.add(c7);
-		customerList.add(c8);
-	}
+	@Autowired
+	CustomersRepository repo;
 
 	@GetMapping
-	public Collection<Customer> getAll() {
-		return this.customerList;
+	public Iterable<Customer> getAll() {
+		return repo.findAll();
 	}
 
 	@GetMapping("/{customerId}")
-	public Customer getCustomerById(@PathVariable("customerId") long id) {
-
-		Customer customer = null;
-		for (int i = 0; i < customerList.size(); i++) {
-			if (customerList.get(i).getId() == id) {
-				customer = customerList.get(i);
-			}
-		}
-		return customer;
+	public Optional<Customer> getCustomerById(@PathVariable("customerId") long id) {
+		//return repo.findOne(id);
+		return repo.findById(id);
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri) {
+		if (newCustomer.getId() != 0 || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+			// Reject we'll assign the customer id
+			return ResponseEntity.badRequest().build();
+		}
+		newCustomer = repo.save(newCustomer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newCustomer.getId()).toUri();
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
+		return response;
+	}
+
+	//lookupCustomerByName GET
+	@GetMapping("/byname/{username}")
+	public ResponseEntity<?> lookupCustomerByNameGet(@PathVariable("username") String username,
+			UriComponentsBuilder uri) {
+//		ApiLogger.log("username: " + username);
+		
+		Iterator<Customer> customers = repo.findAll().iterator();
+		while(customers.hasNext()) {
+			Customer cust = customers.next();
+			if(cust.getName().equalsIgnoreCase(username)) {
+				ResponseEntity<?> response = ResponseEntity.ok(cust);
+				return response;				
+			}			
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+	
+	//lookupCustomerByName POST
+	@PostMapping("/byname")
+	public ResponseEntity<?> lookupCustomerByNamePost(@RequestBody String username, UriComponentsBuilder uri) {
+		//ApiLogger.log("username: " + username);
+		Iterator<Customer> customers = repo.findAll().iterator();
+		while(customers.hasNext()) {
+			Customer cust = customers.next();
+			if(cust.getName().equals(username)) {
+				ResponseEntity<?> response = ResponseEntity.ok(cust);
+				return response;				
+			}			
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}	
+	
+	
+	@PutMapping("/{customerId}")
+	public ResponseEntity<?> putCustomer(
+			@RequestBody Customer newCustomer,
+			@PathVariable("customerId") long customerId) 
+	{
+		if (newCustomer.getId() != customerId || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		newCustomer = repo.save(newCustomer);
+		return ResponseEntity.ok().build();
+	}	
+	
+	@DeleteMapping("/{customerId}")
+	public ResponseEntity<?> deleteCustomerById(@PathVariable("customerId") long id) {
+		// repo.delete(id);
+		repo.deleteById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}	
+	
+	
 }
